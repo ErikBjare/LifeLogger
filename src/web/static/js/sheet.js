583c36a3e3d4fd23a2775905bdea790a8f2bffa6
Erik Bjäreholt <erik.bjareholt@gmail.com>
2014-05-19 18:45:52 +0200

    Added a bit of backbone.js and underscore, much WIP, such libraries, very wow.

--- src/web/static/js/sheet.js
+++ src/web/static/js/sheet.js
@@ -1,24 +1,54 @@
+var SheetModel = Backbone.Model.extend({
+    idAttribute: "_id"
+});
+
+var SheetView = Backbone.View.extend({
+  tagName: "table",
+  className: "table",
+
+//  template: _.template(),
+
+  events: {
+    "click td.cell":    "editCell"
+  },
+
+  initialize: function() {
+    this.on("open", this.open, this)
+    //this.listenTo(this.model, "change", this.render);
+  },
+
+  render: function() {
+    this.$el.html("<div>Nothing to see here!</div>")
+    return this;
+  },
+
+  editCell: function(e) {
+      target = $(e.target);
+      var text = target.text();
+      target.text('');
+      function save_cell() {
+          var newText = $('#curr_input').val();
+          target.text(newText).parent().find('input').remove();
+      }
+      textbox = $('<input type="text" class="input" id="curr_input"/>');
+      textbox.appendTo(target).val(text).select();
+      textbox.blur(save_cell);
+      textbox.bind("enterKey", save_cell);
+      textbox.keyup(function(e){
+          if(e.keyCode == 13) $(this).trigger("enterKey");
+      });
+  }
+});
+
 
 function cell_onclick(e) {
-    e.target = $(e.target);
-    var text = e.target.text();
-    e.target.text('');
-    function save_cell() {
-        var newText = $('#curr_input').val();
-        e.target.text(newText).parent().find('input').remove();
-    }
-    textbox = $('<input type="text" class="input" id="curr_input"/>');
-    textbox.appendTo(e.target).val(text).select();
-    //textbox.blur(save_cell);
-    textbox.bind("enterKey", save_cell);
-    textbox.keyup(function(e){
-        if(e.keyCode == 13) $(this).trigger("enterKey");
-    });
 }
 
 function init() {
     cells = $("td.cell");
     cells.click(cell_onclick);
+    var sheet = new SheetView({id: "sheet", el: document.getElementById("sheet")});
+    console.log(sheet.$el)
 }
 
 
